ae1a78da68380e770d550c1e365c5b9a05f130cf
alvaro.rodrigues.glo <Claro@123>
2020-11-30 20:51:18 -0300

    Publishing projects webview to github

---
+++ levegamesecommerce/app/src/main/java/com/levegames/ecommerce/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.levegames.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webview.webViewClient = WebViewClient()
+        webview.settings.javaScriptEnabled = true
+
+        webview.loadUrl("http://10.0.0.104:8080/Home")
+    }
+}
